51582dfd40683f5a78178e911c90e1b56cdc9b3f
Sinan Mutlu <smutludev@gmail.com>
2026-02-11 11:38:17 +0300

    feat(Z3-6052): add/send discount-code-entry flag

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -104,6 +104,9 @@ export interface IZotloCheckoutParams {
   /** (Optional) The language code for the checkout form, e.g., `en`, `fr`, `pt_br`. */
   language?: string;
 
+  /** (Optional) Enable or disable the discount code entry field in the checkout form. Default is `false`. */
+  enableDiscountCodeEntry?: boolean;
+
   /** You can customize your form on config with style parameter. If you do not define any parameters, the settings made in the Zotlo Console will apply by default. */
   style?: IZotloCheckoutStyle;
 
@@ -264,6 +267,7 @@ export type FormSetting = {
     max: string | number;
     stepSize: string | number;
   };
+  enableDiscountCodeEntry?: boolean;
 };
 
 export type PackageData = {
